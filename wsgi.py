66c954b616cf9869c7e7c9e1acd9cbe08ff39e7b
Lorenzo <lorenzo.it@outlook.com>
2018-04-25 13:34:09 +0200

    OS Flask Metro

--- wsgi.py
+++ wsgi.py
@@ -12,7 +12,7 @@ def init_table():
     url = r"https://en.wikipedia.org/wiki/List_of_stations_of_the_Paris_M%C3%A9tro"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
-    connection = connect_postgres()
+    connection = connect_postgres_oc()
     cursor = connection.cursor()
 
     table = soup.find('table')
@@ -34,33 +34,34 @@ def init_table():
                     #print(lignes)
             index += 1
             data.append([station, lignes])
-
-
     # extrait des infos
-    for x in data:
-        for t in x[1]:
-            for i in t:
-                try:
-                    cursor.execute("""INSERT INTO metro_data (name,ligne) 
-                           VALUES (%s,%s::integer)""", (x[0], i))
-                except (Exception, psycopg2.DatabaseError) as error:
-                    val = error
-                connection.commit()
-    return val
+    # for x in data:
+    #     for t in x[1]:
+    #         for i in t:
+    #             try:
+    #                 cursor.execute("""INSERT INTO metro_data (name,ligne)
+    #                        VALUES (%s,%s::integer)""", (x[0], i))
+    #             except (Exception, psycopg2.DatabaseError) as error:
+    #                 val = error
+    #             connection.commit()
 
-# def connect_postgres():
-#     try:
-#         connect_str = "dbname='metro' user='metro' host='localhost' " + \
-#         "password='metro'"
-#         print("Connecting to database\n	->%s" % (connect_str))
-#         # use our connection values to establish a connection
-#         conn = psycopg2.connect(connect_str)
-#         print("Connected!\n")
-#     except Exception as e:
-#         print("Uh oh, can't connect. Invalid dbname, user or password?")
-#     return conn
+    cursor.execute("select * from metro_data;")
+    result = cursor.fetchall()
+    return result
 
 def connect_postgres():
+    try:
+        connect_str = "dbname='metro' user='metro' host='localhost' " + \
+        "password='metro'"
+        print("Connecting to database\n	->%s" % (connect_str))
+        # use our connection values to establish a connection
+        conn = psycopg2.connect(connect_str)
+        print("Connected!\n")
+    except Exception as e:
+        print("Uh oh, can't connect. Invalid dbname, user or password?")
+    return conn
+
+def connect_postgres_oc():
     try:
         dbname = os.environ.get("POSTGRESQL_DATABASE","NOT FOUND")
         user = os.environ.get("POSTGRESQL_USER","NOT FOUND")
@@ -76,8 +77,8 @@ def connect_postgres():
 
 @application.route("/")
 def hello():
-    nb_stations = init_table()
-    return 'Nombre de stations : {}'.format(nb_stations)
+    stations = init_table()
+    return 'Liste des stations : {}'.format(stations)
 
 if __name__ == "__main__":
     application.run()
