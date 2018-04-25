fbdfb8969006f02ee13288fb7fca6745fcfc6039
Lorenzo <lorenzo.it@outlook.com>
2018-04-25 10:10:39 +0200

    OS Flask Metro

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 # -- How to set 2 apps in Openshift
 from bs4 import BeautifulSoup
 import requests
+import psycopg2
 from flask import Flask
 
 application = Flask(__name__)
@@ -12,6 +13,10 @@ def init_table():
     table = soup.find('table')
     data = []
     nbr_stations = 0
+
+    connection = connect_postgres()
+    cursor = connection.cursor()
+
     for tr in table.findAll('tr'):
         index = 0
         station = ""
@@ -19,14 +24,51 @@ def init_table():
         for td in tr.findAll('td')[::3]:
             for a in td.findAll('a'):
                 if index == 0:
+                    index += 1
                     station = a.text
                     nbr_stations += 1
+                    #print(station)
                 else:
                     lignes.append(a.text)
-                    # print(lignes)
+                    #print(lignes)
                     index += 1
-        data.append([station, lignes])
+                    data.append([station, lignes])
+
+    print(data)
+
+    for x in data:
+        for t in x[1]:
+            if len(x[1]) > 1:
+                for i in t:
+                    try:
+                        cursor.execute("""INSERT INTO metro_data (name,ligne)
+                        VALUES (%s,%s::integer)""", (x[0], i))
+                    except (Exception, psycopg2.DatabaseError) as error:
+                        val = error
+                    connection.commit()
+            else:
+                try:
+                    cursor.execute("""INSERT INTO metro_data (name,ligne)
+                    VALUES (%s,%s::integer)""", (x[0], x[1]))
+                except (Exception, psycopg2.DatabaseError) as error:
+                    val = error
+                connection.commit()
+
     return nbr_stations
+
+def connect_postgres():
+    try:
+        connect_str = "dbname='metro' user='metro' host='localhost' " + \
+        "password='metro'"
+        print("Connecting to database\n	->%s" % (connect_str))
+        # use our connection values to establish a connection
+        conn = psycopg2.connect(connect_str)
+        print("Connected!\n")
+    except Exception as e:
+        print("Uh oh, can't connect. Invalid dbname, user or password?")
+    return conn
+
+
 
 @application.route("/")
 def hello():
