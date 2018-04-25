278010d90dbdf4bb11b563ab044ea97d0abbf0bc
Lorenzo <lorenzo.it@outlook.com>
2018-04-25 11:36:48 +0200

    OS Flask Metro

--- wsgi.py
+++ wsgi.py
@@ -12,51 +12,41 @@ def init_table():
     url = r"https://en.wikipedia.org/wiki/List_of_stations_of_the_Paris_M%C3%A9tro"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
-    table = soup.find('table')
-    data = []
-    nbr_stations = 0
-
     connection = connect_postgres()
     cursor = connection.cursor()
 
+    table = soup.find('table')
+
+    data = []
+    nbr_stations = 0
     for tr in table.findAll('tr'):
+
         index = 0
         station = ""
         lignes = []
         for td in tr.findAll('td')[::3]:
             for a in td.findAll('a'):
                 if index == 0:
-                    index += 1
                     station = a.text
-                    nbr_stations += 1
-                    #print(station)
+                    nbr_stations +=1
                 else:
                     lignes.append(a.text)
                     #print(lignes)
-                    index += 1
-                    data.append([station, lignes])
+            index += 1
+            data.append([station, lignes])
 
-    print(data)
 
+    # extrait des infos
     for x in data:
         for t in x[1]:
-            if len(x[1]) > 1:
-                for i in t:
-                    try:
-                        cursor.execute("""INSERT INTO metro_data (name,ligne)
-                        VALUES (%s,%s::integer)""", (x[0], i))
-                    except (Exception, psycopg2.DatabaseError) as error:
-                        val = error
-                    connection.commit()
-            else:
+            for i in t:
                 try:
-                    cursor.execute("""INSERT INTO metro_data (name,ligne)
-                    VALUES (%s,%s::integer)""", (x[0], x[1]))
+                    cursor.execute("""INSERT INTO metro_data (name,ligne) 
+                           VALUES (%s,%s::integer)""", (x[0], i))
                 except (Exception, psycopg2.DatabaseError) as error:
                     val = error
                 connection.commit()
-
-    return nbr_stations
+    return val
 
 # def connect_postgres():
 #     try:
@@ -72,7 +62,7 @@ def init_table():
 
 def connect_postgres():
     try:
-        dbname = os.environ.get("POSTGRESQL_DBNAME","NOT FOUND")
+        dbname = os.environ.get("POSTGRESQL_DATABASE","NOT FOUND")
         user = os.environ.get("POSTGRESQL_USER","NOT FOUND")
         host = os.environ.get("POSTGRESQL_SERVICE_HOST","NOT FOUND")
         password = os.environ.get("POSTGRESQL_PASSWORD","NOT FOUND")
